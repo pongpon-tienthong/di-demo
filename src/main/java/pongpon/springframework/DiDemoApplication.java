2e643300ee6c5f2e0d730a1269600edb11612680
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2017-10-25 23:03:01 -0500

    Test creating bean

--- src/main/java/pongpon/springframework/DiDemoApplication.java
+++ src/main/java/pongpon/springframework/DiDemoApplication.java
@@ -2,11 +2,15 @@ package pongpon.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import pongpon.springframework.controllers.MyController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
+        MyController myController = (MyController) context.getBean("myController");
+        myController.hello();
+    }
 }
---
+++ src/main/java/pongpon/springframework/controllers/MyController.java
@@ -0,0 +1,11 @@
+package pongpon.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String hello() {
+        System.out.println("Hello World");
+        return "Run ...";
+    }
+}
