63b10103bd96831894cb68387c588ea449750828
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2017-11-02 12:27:48 -0500

    Spring Profile Properties

--- src/main/java/pongpon/springframework/configs/PropertyConfig.java
+++ src/main/java/pongpon/springframework/configs/PropertyConfig.java
@@ -3,7 +3,6 @@ package pongpon.springframework.configs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import pongpon.springframework.fakes.FakeDataSource;
 import pongpon.springframework.fakes.JmsFakeDataSource;
 
